2725baac25b6296120e8d2cf71480eed113007eb
Mark Howe <mvahowe@gmail.com>
2020-06-28 15:45:17 +0200

    Command line options

--- scripts/import.js
+++ scripts/import.js
@@ -2,12 +2,9 @@
 const usfmjs = require('usfm-js');
 const fse = require('fs-extra');
 const xre = require('xregexp');
+const path = require('path');
 
 // node import.js <usfmPath>
-
-// Read USFM using USFM-JS
-
-const usfm = fse.readFileSync(process.argv[2], "utf-8");
 
 class SAG {
 
@@ -15,7 +12,7 @@ class SAG {
         this.usfmJSON = usfmjs.toJSON(usfm);
         this.headers = this.usfmJSON.headers;
         this.chapterVerses = {};
-        this.paras = [];
+        this.words = {};
         this.tokens = {};
         this.currentChapter = null;
         this.currentVerse = null;
@@ -83,6 +80,11 @@ class SAG {
                         if (matchType === "alphanumeric") {
                             const normalized = this.normalizeWord(match);
                             this.tokens[tokenId].normalizedWord = normalized;
+                            if (normalized in this.words) {
+                                this.words[normalized].tokens.add(tokenId);
+                            } else {
+                                this.words[normalized] = {tokens: new Set([tokenId])};
+                            }
                         }
                         vNode.tokens[tokenId] = true;
                         verseText = rest;
@@ -128,6 +130,10 @@ class SAG {
         return token.text;
     }
 
+    normalizedText(token) {
+        return token.normalizedWord;
+    }
+
     wordTokens(tokens) {
         return tokens.filter(t => t.type === "alphanumeric");
     }
@@ -136,18 +142,85 @@ class SAG {
         return xre("^\\s+").exec(str) ? " " : str;
     }
 
+    normalizeWord(str) {
+        return str.toLowerCase();
+    }
+
+    wordsByFrequency() {
+        const wordArray = Array.from(Object.entries(this.words));
+        wordArray.sort(
+            (a, b) => {
+                if (a[1].tokens.size > b[1].tokens.size) {
+                    return 1;
+                } else if (a[1].tokens.size < b[1].tokens.size) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            }
+        );
+        return wordArray;
+    }
+
+    formatWordCountEntry(kv) {
+        const [word, data] = kv;
+        const tokens = data.tokens;
+        return word + " - " + this.wordCV(tokens).join(", ");
+    }
+
+    wordCV(tokenSet) {
+        const cv = new Set();
+        for (const tokenId of Array.from(tokenSet)) {
+            const token = this.tokens[tokenId];
+            cv.add(token.chapter + ":" + token.verse)
+        }
+        return Array.from(cv);
+    }
+
+}
+
+const usage = function() {
+    const pathBase = path.parse(process.argv[1]).base;
+    console.log("\n* USAGE *");
+    console.log("node", pathBase, "<usfmPath>", "stats");
+    console.log("node", pathBase, "<usfmPath>", "originalText", "23", "1,2,3");
+    console.log("node", pathBase, "<usfmPath>", "alphanumericOnlyText", "23", "1,2,3");
+    console.log("node", pathBase, "<usfmPath>", "wordRoots", "23", "1,2,3");
+    console.log("node", pathBase, "<usfmPath>", "normalizedText", "23", "1,2,3");
+    console.log("node", pathBase, "<usfmPath>", "wordSearch", "faithfulness");
+    console.log("node", pathBase, "<usfmPath>", "frequency", "200");
+}
+
+var usfm;
+try {
+    usfm = fse.readFileSync(process.argv[2], "utf-8");
+} catch (err) {
+    console.log("Could not load USFM:", err);
+    usage();
+    return;
 }
 
 const sag = new SAG(usfm);
 
-console.log("* The original text *");
-console.log(sag.tokensForVerses("1", ["1"]).map(t => sag.tokenText(t)).join(""));
-
-console.log("\n* Words only *");
-console.log(sag.wordTokens(sag.tokensForVerses("1", ["1"])).map(t => sag.tokenText(t)).join(" "));
-
-console.log("\n* Words and punctuation with normalized whitespace *");
-console.log(sag.tokensForVerses("1", ["1"]).map(t => sag.normalizeSpace(sag.tokenText(t))).join(""));
-
-console.log("\n* Several verses *");
-console.log(sag.tokensForVerses("119", ["9", "10", "11", "12", "13", "14", "15", "16"]).map(t => sag.normalizeSpace(sag.tokenText(t))).join(""));
+if (process.argv[3] === "stats") {
+    console.log("Tokens:", Object.keys(sag.tokens).length);
+    console.log("Chapters", Object.keys(sag.chapterVerses).length);
+    console.log("Verses:", Object.values(sag.chapterVerses).map(c => Object.keys(c.verses).length).reduce((acc, v) => acc + v));
+    console.log("Unique Words:", Object.keys(sag.words).length);
+} else if (process.argv[3] === "usfmjs") {
+    console.log(JSON.stringify(sag.usfmJSON, null, 2));
+} else if (process.argv[3] === "originalText") {
+    console.log(sag.tokensForVerses(process.argv[4], process.argv[5].split(",")).map(t => sag.tokenText(t)).join(""));
+} else if (process.argv[3] === "alphanumericOnlyText") {
+    console.log(sag.wordTokens(sag.tokensForVerses(process.argv[4], process.argv[5].split(","))).map(t => sag.tokenText(t)).join(" "));
+} else if (process.argv[3] === "wordRoots") {
+    console.log(sag.wordTokens(sag.tokensForVerses(process.argv[4], process.argv[5].split(","))).map(t => sag.normalizedText(t)).join(" "));
+} else if (process.argv[3] === "normalizedText") {
+    console.log(sag.tokensForVerses(process.argv[4], process.argv[5].split(",")).map(t => sag.normalizeSpace(sag.tokenText(t))).join(""));
+} else if (process.argv[3] === "wordSearch") {
+    console.log(process.argv[4] + " - " + sag.wordCV(sag.words[process.argv[4]].tokens).join(", "));
+} else if (process.argv[3] === "frequency") {
+    console.log(sag.wordsByFrequency().reverse().filter(kv => kv[1].tokens.size > process.argv[4]).map(kv => kv[0] + ": " + kv[1].tokens.size));
+} else {
+    usage();
+}
