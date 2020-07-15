11e84201368eff04664632b5d567be2e488a8bc8
Mark Howe <mvahowe@gmail.com>
2020-07-15 11:58:25 +0200

    Try Gun community fixes

--- server/gunServer.js
+++ server/gunServer.js
@@ -3,24 +3,35 @@ const gun = require('gun');
 const http = require('http');
 const U2T = require("../lib/usfm2tokens").default;
 
-const httpServer = http.createServer().listen(2468);
-const gunServer = gun({web: httpServer});
-const collections = {};
-const toLoad = {
-    "oeb_jol": "../test/test_data/oeb_jol.usfm",
-    "en_ult_psa": "../test/test_data/en_ult_psa.usfm",
-    "en_ult_lam": "../test/test_data/en_ult_lam.usfm"
-};
-
-const gunUW = gunServer.get("unfoldingWord");
-for (const [cName, cPath] of Object.entries(toLoad)) {
-    collections[cName] = new U2T(cPath);
-    const gunCollection = gunUW.get("collections").get(cName);
-    const date = new Date();
-    gunCollection.put({"created": date.toString()});
-    const gunCollectionTokens = gunCollection.get("tokenIds");
-    for (const tokenId of Object.keys(collections[cName].tokens)) {
-        gunCollectionTokens.set({"id": tokenId});
+class TokenServer {
+    constructor () {
+        const httpServer = http.createServer().listen(2468);
+        this.gunServer = gun({web: httpServer});
+        this.collections = {};
+        this.toLoad = {
+            "oeb_jol": "../test/test_data/oeb_jol.usfm",
+            "en_ult_lam": "../test/test_data/en_ult_lam.usfm",
+            "en_ult_psa": "../test/test_data/en_ult_psa.usfm",
+        };
     }
-    console.log(cName, "added");
+
+    async populateCollections() {
+        const gunUW = this.gunServer.get("unfoldingWord");
+        let count = 0;
+        for (const [cName, cPath] of Object.entries(this.toLoad)) {
+            this.collections[cName] = new U2T(cPath);
+            const gunCollection = gunUW.get("collections").get(cName);
+            const date = new Date();
+            gunCollection.put({"created": date.toString()});
+            const gunCollectionTokens = gunCollection.get("tokenIds");
+            for (const tokenId of Object.keys(this.collections[cName].tokens)) {
+                await gunCollectionTokens.put({"id": tokenId});
+                console.log(count++);
+            }
+            console.log(cName, "added");
+        }
+    }
 }
+
+const server = new TokenServer();
+server.populateCollections().then();
