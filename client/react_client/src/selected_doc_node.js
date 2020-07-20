ad30fc6b2f2326187e4435b638d4e0e64e84b7b1
Mark Howe <mvahowe@gmail.com>
2020-07-20 16:15:34 +0200

    Word in verses

--- client/react_client/src/selected_doc_node.js
+++ client/react_client/src/selected_doc_node.js
@@ -2,13 +2,14 @@ import React, {Component} from "react";
 import StatsView from "./stats_view";
 import VersificationView from "./versification_view";
 import FindVersesView from "./find_verses_view";
+import WordInVersesView from "./word_in_verses_view";
 
 class SelectedNode extends Component {
 
     constructor() {
         super();
         this.state = {
-            nodeView: "findVerses"
+            nodeView: "wordInVerses"
         }
     }
 
@@ -35,6 +36,15 @@ class SelectedNode extends Component {
             case "findVerses":
                 return (
                     <FindVersesView
+                        language={this.props.language}
+                        translation={this.props.translation}
+                        docId={this.props.docId}
+                        docRecord={this.props.docRecord}
+                    />
+                )
+            case "wordInVerses":
+                return (
+                    <WordInVersesView
                         language={this.props.language}
                         translation={this.props.translation}
                         docId={this.props.docId}
---
+++ client/react_client/src/word_in_verses_view.js
@@ -0,0 +1,54 @@
+import React, {Component} from "react";
+
+class WordInVersesView extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            word: ""
+        }
+    }
+
+    updateFormValues(event) {
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    render() {
+        const now = new Date().getTime();
+        const graph = this.props.docRecord.graph;
+        return (
+            <div className="row">
+                <div className="col p-3 nodeView">
+                    <div className="row">
+                        <div className="col h4 text-primary">Find Word in Verses</div>
+                    </div>
+                    <div className="form-row">
+                        <div className="col">
+                            <input name="word" type="text" className="form-control" placeholder="word"
+                                   onChange={(e) => this.updateFormValues(e)}/>
+                        </div>
+                    </div>
+                    {
+                        (this.state.word !== "") && (this.state.word in graph.words) ?
+                            graph.wordInVerses(this.state.word).map(
+                                wiv => <div className="row">
+                                    <div className="col">
+                                        {wiv}
+                                    </div>
+                                </div>
+                            )
+                            :
+                            ""
+                    }
+                    <div className="row">
+                        <div className="col text-secondary text-right"><small>Rendered
+                            in {new Date().getTime() - now} msec</small></div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default WordInVersesView;
